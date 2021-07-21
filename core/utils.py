2837da0c3946df67ff1b4b8910f457a82c39c23c
yerang <grondin0425@gmail.com>
2021-07-21 21:43:24 +0900

    Add: users app add SignIn/Up View with KakaoApi add Decorator(login_required) add users/tests.py => test kakao-signin/up

---
+++ core/utils.py
@@ -0,0 +1,32 @@
+import jwt
+
+from django.http import JsonResponse
+
+from users.models import User
+
+from my_settings import SECRET_KEY, ALGORITHM
+
+def login_required(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization')
+            pay_load     = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+            user         = User.objects.get(id=pay_load['user_id'])
+            request.user = user
+
+            return func(self, request, *args, **kwargs)
+
+        except jwt.InvalidTokenError:
+            return JsonResponse({'message': 'INVALID_TOKEN'}, status=401)
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message': 'DECODE_ERROR'}, status=400)
+        except jwt.ExpiredSignatureError:
+            return JsonResponse({'message': 'EXPIRED_TOKEN'}, status=401)
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'USER_DOES_NOT_EXISTS'}, status=401)
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        except Exception as error:
+            return JsonResponse({'message': error}, status=400)
+
+    return wrapper
