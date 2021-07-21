1bddc5f7a392176e89c73a7eed51c6a89e0a3953
yerang <grondin0425@gmail.com>
2021-07-21 21:43:24 +0900

    Add: users app add SignIn/Up View with KakaoApi add Decorator(login_required) add users/tests.py => test kakao-signin/up

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+import jwt
 
-# Create your tests here.
+from django.test   import TestCase, Client
+from unittest.mock import patch, MagicMock
+
+from users.models import User
+from my_settings  import SECRET_KEY, ALGORITHM
+
+class UserTest(TestCase):
+    def setup(self):
+        User.objects.create(
+            id        = 1,
+            kakao_id  = "1816852310",
+            email     = "yrchic@hotmail.com",
+            nick_name = "yr K",
+            birthday  = "0425",
+            gender    = "male"
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    @patch('users.views.requests')
+    def test_kakao_signin_new_user_success(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id": "123456789",
+                    "kakao_account": { 
+                        "email": "blahblah@blah.com",
+                        "birthday": "0425",
+                        "gender": "male",
+                    },
+                    "properties": {
+                        "nickname": "blahblahblah"
+                    }
+                }
+
+        mocked_requests.post = MagicMock(return_value = MockedResponse())
+        headers              = {"Authorization": "test_token"}
+        response             = client.get('/users/signin/kakao', headers=headers)
+
+        access_token = response.json().get('access_token')
+        pay_load     = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        user         = User.objects.get(id=pay_load['user_id'])
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(user.kakao_id, "123456789")
+
+    @patch('users.views.requests')
+    def test_kakao_signin_exist_user_success(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id": "1816852310",
+                    "kakao_account": { 
+                        "email": "yrchic@hotmail.com",
+                        "birthday": "0425",
+                        "gender": "male",
+                    },
+                    "properties": {
+                        "nickname": "yr K"
+                    }
+                }
+
+        mocked_requests.post = MagicMock(return_value = MockedResponse())
+        headers              = {"Authorization": "test_token"}
+        response             = client.get('/users/signin/kakao', headers=headers)
+
+        access_token = jwt.encode({'user_id': 1}, SECRET_KEY, algorithm=ALGORITHM)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json().get('access_token'), access_token)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import json, requests, jwt
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+
+from users.models import User
+
+from my_settings import SECRET_KEY, ALGORITHM
+
+class KakaoSignInView(View):
+    def get(self, request):
+        try:
+            kakao_access_token = request.headers.get('Authorization')
+
+            headers = {'Authorization': f'Bearer {kakao_access_token}'}
+
+            kakao_user_info = requests.post('https://kapi.kakao.com/v2/user/me', headers=headers).json()
+
+            kakao_id  = kakao_user_info['id']
+            email     = kakao_user_info['kakao_account']['email']
+            nick_name = kakao_user_info['properties']['nickname']
+            birthday  = kakao_user_info['kakao_account']['birthday']
+            gender    = kakao_user_info['kakao_account']['gender']
+
+            user, is_created =  User.objects.get_or_create(kakao_id = kakao_id)
+            
+            if is_created:
+                user.email     = email
+                user.nick_name = nick_name
+                user.birthday  = birthday
+                user.gender    = gender
+                user.save()
+
+            access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, algorithm=ALGORITHM)
+
+            return JsonResponse({'access_token': access_token}, status=200)
+        
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        except Exception as error:
+            return JsonResponse({'message': error}, status=400)
