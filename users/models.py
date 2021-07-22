c38848018890a883cc5c77088e6baee01cec1adb
yerang <grondin0425@gmail.com>
2021-07-22 09:35:16 +0900

    FIX: users/models.py: password column => null True , birthday column => charfiled

--- users/models.py
+++ users/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 from core.models import SoftDeleteModel, TimeStampModel
@@ -6,9 +5,9 @@ from core.models import SoftDeleteModel, TimeStampModel
 class User(TimeStampModel, SoftDeleteModel):
     kakao_id      = models.CharField(max_length=20, unique=True)
     email         = models.EmailField(max_length=30, unique=True)
-    password      = models.CharField(max_length=200)
+    password      = models.CharField(max_length=200, null=True)
     nick_name     = models.CharField(max_length=20)
-    birthday      = models.DateField()
+    birthday      = models.CharField(max_length=20)
     gender        = models.CharField(max_length=10)
     address       = models.CharField(max_length=30, null=True)
     reviewed_arts = models.ManyToManyField('arts.Art', through='reviews.Review', related_name='reviewed_users')
