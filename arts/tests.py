0c57a219bfff54066682903d2f2725b8be6a713a
hyeonjung choi <waithjno@gmail.com>
2021-07-28 17:06:53 +0900

    수정:artview

--- arts/tests.py
+++ arts/tests.py
@@ -1,3 +1,232 @@
-from django.test import TestCase
+from django.test    import TestCase
+from django.test    import Client
 
-# Create your tests here.
+from arts.models    import Art, Artist, Size, Shape, Color, ArtColor, Theme, ArtTheme
+from carts.models   import Cart 
+from reviews.models import ReviewImage, Review
+from users.models   import User
+
+class ArtsTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id = 1,
+            email = "a@gmail.com",
+            password = 1234,
+            nick_name = "졸려",
+            birthday = "2000-01-01",
+            gender = "male",
+            kakao_id = "1111"
+        )
+
+        Artist.objects.create(
+            id = 1,
+            name = "최현정"
+        )
+
+        Size.objects.create(
+            id = 1,
+            name = 1
+        )
+        
+        Size.objects.create(
+            id = 2,
+            name = 2,
+        )
+
+        Shape.objects.create(
+            id = 1,
+            name = "가로형"
+        )
+
+        Shape.objects.create(
+            id = 2,
+            name = "세로형"
+        )
+
+        Art.objects.create(
+            id = 1,
+            title = "하루종일 잠만 자고 싶어",
+            image_url = "https://pbs.twimg.com/profile_images/1383818413982715911/SewPb-Gm_400x400.jpg",
+            description = "에어컨 틀고 이불안에서 12시간 자고 싶어",
+            price = "150000.00",
+            artist_id = 1,
+            size_id = 1,
+            shape_id= 1,
+        )
+
+
+        Art.objects.create(
+            id = 2,
+            title = "오늘은 짜빠게티 요리사",
+            image_url = "https://t1.daumcdn.net/liveboard/realfood/ad5e6801850747b29c781aa9e4d57201.JPG",
+            description = "룰루",
+            price = "150000.00",
+            artist_id = 1,
+            size_id = 1,
+            shape_id= 2,
+        )
+
+        Color.objects.create(
+            id = 1,
+            name = "그린"
+        )
+        Color.objects.create(
+            id = 2,
+            name = "레드"
+        )
+
+        Theme.objects.create(
+            id = 1,
+            name = "풍경"
+        )
+
+        ArtColor.objects.create(
+            # id = id,
+            art_id = 1,
+            color_id = 1
+        )
+        ArtColor.objects.create(
+            # id = id,
+            art_id = 2,
+            color_id = 2
+        )
+        
+        ArtTheme.objects.update_or_create(
+            id = 1,
+            art_id = 1,
+            theme_id = 1
+        )
+
+        Cart.objects.update_or_create(
+            id = 1,
+            user_id = 1,
+            art_id = 1
+        )
+
+        Review.objects.update_or_create(
+            id = 1,
+            art_id = 1,
+            comment = "공감합니다~",
+            user_id = 1
+        )
+        ReviewImage.objects.update_or_create(
+            id = 1,
+            image_url = "https://storage.googleapis.com/jjalbot-jjals/2018/12/aziS307P5/zzal.jpg", 
+            review_id = 1
+        )
+
+
+    def tearDown(self):
+        ReviewImage.objects.all().delete()
+        Review.objects.all().delete()
+        Cart.objects.all().delete()
+        ArtTheme.objects.all().delete()
+        ArtColor.objects.all().delete()
+        Theme.objects.all().delete()
+        Color.objects.all().delete()
+        Art.objects.all().delete()
+        Shape.objects.all().delete()
+        Size.objects.all().delete()
+        Artist.objects.all().delete()
+        User.objects.all().delete()
+
+
+    def test_art_get_view(self):
+        client = Client()
+        response = client.get('/arts')
+
+        self.assertEqual(response.status_code,200)
+        
+        self.assertEqual(response.json(),
+        {
+        'results': 
+                [
+                    {
+                        'art_id': 1, 
+                        'title': '하루종일 잠만 자고 싶어', 
+                        'image_url': 'https://pbs.twimg.com/profile_images/1383818413982715911/SewPb-Gm_400x400.jpg', 'artist_name': '최현정',
+                        'artist_id': 1, 
+                        'size': 1, 
+                        'is_available': True, 
+                        'price': '150000.00', 
+                        'description': '에어컨 틀고 이불안에서 12시간 자고 싶어', 
+                        'shape': '가로형'
+                    }, 
+                    {
+                        'art_id': 2,
+                        'title': '오늘은 짜빠게티 요리사', 
+                        'image_url': 'https://t1.daumcdn.net/liveboard/realfood/ad5e6801850747b29c781aa9e4d57201.JPG', 
+                        'artist_name': '최현정', 
+                        'artist_id': 1, 
+                        'size': 1, 
+                        'is_available': True, 
+                        'price': '150000.00', 
+                        'description': '룰루', 
+                        'shape': '세로형'
+                    }
+                ], 
+                'total_count': 2
+            })
+
+    def test_art_filter_once_get_view(self):
+        client = Client()
+        response = client.get('/arts?color=레드')
+
+        self.assertEqual(response.status_code,200)
+    
+        self.assertEqual(response.json(),
+        {
+        'results': 
+                [
+                    {
+                    'art_id': 2,
+                    'title': '오늘은 짜빠게티 요리사', 
+                    'image_url': 'https://t1.daumcdn.net/liveboard/realfood/ad5e6801850747b29c781aa9e4d57201.JPG', 
+                    'artist_name': '최현정', 
+                    'artist_id': 1, 
+                    'size': 1, 
+                    'is_available': True, 
+                    'price': '150000.00', 
+                    'description': '룰루', 
+                    'shape': '세로형'
+                    }
+                ], 
+                'total_count': 1
+            })
+
+    def test_art_filter_twice_get_view(self):
+        client = Client()
+        response = client.get('/arts?color=그린&min_price=0&max_price=150000')
+
+        self.assertEqual(response.status_code,200)
+        
+        self.assertEqual(response.json(),
+        {
+        'results': 
+                [
+                    {
+                    'art_id': 1, 
+                    'title': '하루종일 잠만 자고 싶어', 
+                    'image_url': 'https://pbs.twimg.com/profile_images/1383818413982715911/SewPb-Gm_400x400.jpg', 
+                    'artist_name': '최현정', 
+                    'artist_id': 1, 
+                    'size': 1, 
+                    'is_available': True, 
+                    'price': '150000.00', 
+                    'description': '에어컨 틀고 이불안에서 12시간 자고 싶어', 
+                    'shape': '가로형'
+                    }
+                ], 
+                'total_count': 1
+            })
+
+    def test_art_filter_get_view_fail(self):
+        client = Client()
+        response = client.get('/arts?limit=[실패]')
+
+        self.assertEqual(response.status_code,400)
+        
+        self.assertEqual(response.json(),
+        {
+            "message": "VALUE_ERROR"
+        })
--- arts/views.py
+++ arts/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.views     import View
+from django.http      import JsonResponse
+from django.db.models import Q
 
-# Create your views here.
+from arts.models      import Art
+
+class ArtsView(View):
+    def get(self,request):
+        try:
+            shape_name_list  = request.GET.getlist('shape', None) 
+            color_name_list  = request.GET.getlist('color', None) 
+            theme_name_list  = request.GET.getlist('theme', None)
+            min_price        = request.GET.get('min_price', None)
+            max_price        = request.GET.get('max_price', None)
+            min_size         = request.GET.get('min_size', None)
+            max_size         = request.GET.get('max_size', None) 
+            artist_name      = request.GET.get('artist', None)
+            sort             = request.GET.get('sort', None)
+            offset           = int(request.GET.get('offset', 1)) 
+            limit            = int(request.GET.get('limit', Art.objects.count()))
+
+
+            sort_dict = { 
+                'id'              : 'id',
+                'size-ascend'     : 'size_id', 
+                'created-descend' : '-created_at',
+                'price-ascend'    : 'price'
+            }
+
+            q = Q()
+
+            if artist_name:
+                q.add(Q(artist__name = artist_name), q.AND)
+
+            if shape_name_list:
+                q.add(Q(shape__name__in = shape_name_list), q.AND)
+
+            if theme_name_list:
+                q.add(Q(themes__name__in = theme_name_list), q.AND)
+
+            if color_name_list:
+                q.add(Q(colors__name__in = color_name_list), q.AND)
+
+            if min_price and max_price:
+                q.add(Q(price__range = (min_price, max_price)), q.AND)
+
+            if min_size and max_size:
+                q.add(Q(size__name__range = (min_size, max_size)), q.AND)
+
+            arts = Art.objects.filter(q).distinct().order_by(sort_dict.get(sort, 'id'))
+            total_count = arts.count()
+
+            results = [
+                {
+                'art_id'           : art.id,
+                'title'        : art.title,
+                'image_url'    : art.image_url,
+                'artist_name'  : art.artist.name,
+                'artist_id'    : art.artist.id,
+                'size'         : art.size.name,
+                'is_available' : art.is_available,
+                'price'        : art.price,
+                'description'  : art.description,
+                'shape'        : art.shape.name,
+                'price'        : art.price,
+                } for art in arts[(offset-1)*limit : (limit*offset)]
+            ]
+            return JsonResponse({"results": results, "total_count" : total_count}  , status = 200)
+        
+        except ValueError:
+            return JsonResponse({"message" : "VALUE_ERROR"} , status = 400)
